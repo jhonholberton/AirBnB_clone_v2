6086fae4385b7b700f653575d67b6443138f1ce1
Andres.Pardo <carlos.pardo@uptc.edu.co>
2022-05-17 18:34:45 -0500

    2 again

--- models/state.py
+++ models/state.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-"""This is the state class"""
-import os
-import models
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
